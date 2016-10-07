928cb413c7439b48c9c61827490cfd36a973ccac
David Neubauer <davidneub@gmail.com>
2016-10-07 17:24:40 +0200

    Add resize listener

--- js-version/index.js
+++ js-version/index.js
@@ -1,33 +1,61 @@
 (function () {
-  var gridNodes = document.getElementsByClassName('grid');
-  var columnSpacing = 20;
-  var columnCount = 3;
 
-  [].forEach.call(gridNodes, function (grid) {
-    var gridWidth = grid.clientWidth;
-    var articles = grid.children;
+  function renderPinterestGrid(columnSpacing, columnCount) {
+    var gridNodes = document.getElementsByClassName('grid');
 
-    var articleWidth = parseInt((gridWidth - columnSpacing * columnCount ) / columnCount, 10);
+    [].forEach.call(gridNodes, function (grid) {
+      var gridWidth = grid.clientWidth;
+      var articles = grid.children;
 
-    var fromLeft = 0;
-    var fromTopByRow = [0, 0 , 0];
+      var articleWidth = parseInt((gridWidth - columnSpacing * (columnCount - 1)) / columnCount, 10);
 
-    [].forEach.call(articles, function (article, index) {
-      var currentRow = index % 3;
-      var fromTop = fromTopByRow[currentRow];
-      fromLeft += articleWidth + columnSpacing + currentRow;
+      var fromLeft = 0;
+      var fromTopByRow = [0, 0 , 0];
 
-      fromTopByRow[currentRow] += article.clientHeight + columnSpacing * 2;
-      if (currentRow === 0) {
-        fromLeft = 0;
-      }
+      [].forEach.call(articles, function (article, index) {
+        var currentRow = index % columnCount;
+        var fromTop = fromTopByRow[currentRow];
+        fromLeft += articleWidth;
+        if (columnCount !== 1) fromLeft += columnSpacing;
 
-      article.setAttribute('style',
-        'position: absolute; ' +
-        'left:' + fromLeft + 'px; ' +
-        'top:' + fromTop + 'px; ' +
-        'width:' + articleWidth + 'px;'
-      );
+        fromTopByRow[currentRow] += article.clientHeight + columnSpacing * 2;
+        if (currentRow === 0) {
+          fromLeft = 0;
+        }
+
+        article.setAttribute('style',
+          'position: absolute; ' +
+          'left:' + fromLeft + 'px; ' +
+          'top:' + fromTop + 'px; ' +
+          'width:' + articleWidth + 'px;'
+        );
+      });
     });
-  });
+  }
+
+  var spacing = 20;
+  var columns = 1;
+
+  var breakpoints = {
+    tablet: 560,
+    desktop: 960
+  };
+
+  function updateColumnCount(availableWidth) {
+    if (availableWidth >= breakpoints.tablet && window.innerWidth < breakpoints.desktop) {
+      columns = 2;
+    } else if (window.innerWidth >= breakpoints.desktop) {
+      columns = 3;
+    } else {
+      columns = 1;
+    }
+  }
+
+  updateColumnCount(window.innerWidth);
+  renderPinterestGrid(spacing, columns);
+
+  window.addEventListener('resize', function () {
+    updateColumnCount(window.innerWidth);
+    renderPinterestGrid(spacing, columns);
+  })
 })();
