ec5253b5814201ce156a741f4bcb470a56277688
David Neubauer <davidneub@gmail.com>
2016-10-07 18:15:24 +0200

    Finish mobile nav

--- navigation/index.js
+++ navigation/index.js
@@ -4,5 +4,17 @@
   var navigationActive = 'nav-active';
   button.addEventListener('click', function () {
     body.className = body.className !== navigationActive ? navigationActive : '';
-  })
+  });
+
+  var dropdownActive = 'nav__item--dropdown is-active';
+  function dropdownClick(e) {
+    var parentNode = e.currentTarget.parentNode;
+    parentNode.className = parentNode.className !== dropdownActive ? dropdownActive : 'nav__item--dropdown';
+  }
+
+  var dropdownButtons = document.getElementsByClassName('nav__dropdown');
+  [].forEach.call(dropdownButtons, function (dropdownButton) {
+    dropdownButton.addEventListener('click', dropdownClick);
+  });
+
 })();
